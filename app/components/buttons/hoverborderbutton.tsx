ea3e92f7bee521cfe87fa23ec0c99a8679b51e7e
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2025-01-13 21:14:05 +0100

    navigation and framer and button

---
+++ app/components/buttons/hoverborderbutton.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
+
+export function HoverBorderButton() {
+  return (
+    <div className="">
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="bg-black  text-white  "
+      >
+      
+        <span>Aceternity UI</span>
+      </HoverBorderGradient>
+    </div>
+  );
+}
---
+++ app/header/nav.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react'
+import { HoverBorderButton } from '../components/buttons/hoverborderbutton'
+
+export default function Nav() {
+  return (
+<nav className="fixed w-full top-0 z-10 bg-[#18181A] backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-700">
+  <div className="max-w-7xl mx-auto px-4 text-white">
+    <div className="flex items-center justify-between h-16">
+      <span className="text-2xl  font-semibold">Logo</span>
+      <div className="flex space-x-4 ">
+        <a href="#">Dashboard</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
+      <HoverBorderButton />
+    </div>
+  </div>
+</nav>
+  )
+}

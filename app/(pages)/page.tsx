875995966ccb53e99ea0cbdcb9475502dc5bc1f0
lucasjolibois54 <lucasjolibois@icloud.com>
2025-01-16 16:35:51 +0100

    feat: subhero

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Home/hero/hero";
+import Subhero from "../components/Home/subhero/Subhero";
 
 export default function Home() {
   return (
     <div className=" bg-[#09090A] min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Hero />
+      <Subhero />
     </div>
   );
 }

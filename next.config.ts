6fb1009ce955018a40030903ee2bcd30ad48a05f
lucasjolibois54 <lucasjolibois@icloud.com>
2025-01-22 20:16:50 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com', 'cdn.prod.website-files.com' ],
+    domains: ['images.unsplash.com', 'plus.unsplash.com', 'cdn.prod.website-files.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com' ],
   }
 };
 

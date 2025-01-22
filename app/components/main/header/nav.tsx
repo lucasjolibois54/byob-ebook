f8d1fc6b178be9d71a15fe960daf067b4e4fce78
lucasjolibois54 <lucasjolibois@icloud.com>
2025-01-22 20:57:01 +0100

    Update nav.tsx

--- app/components/main/header/nav.tsx
+++ app/components/main/header/nav.tsx
@@ -4,7 +4,7 @@ import { HoverBorderButton } from '../buttons/hoverborderbutton'
 
 export default function Nav() {
   return (
-<nav className="fixed w-full top-0 z-10 bg-[#18181A] backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-700">
+<nav className="fixed w-full top-0 z-20 bg-[#18181A] backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-700">
   <div className="max-w-7xl mx-auto px-4 text-white">
     <div className="flex items-center justify-between h-16">
       <span className="text-2xl  font-semibold">Logo</span>

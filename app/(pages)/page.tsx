4322479951e67a11af9b5258b3555bd85a968714
lucasjolibois54 <lucasjolibois@icloud.com>
2025-01-22 20:16:48 +0100

    Update page.tsx

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,11 +1,12 @@
 import { InteractiveFaqAccordion } from "../components/Home/faq/InteractiveFaqAccordion";
 import Hero from "../components/Home/hero/hero";
+import NewHero from "../components/Home/new-hero/NewHero";
 import Subhero from "../components/Home/subhero/Subhero";
 
 export default function Home() {
   return (
     <div className=" bg-[#09090A] min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <Hero />
+      <NewHero />
       <Subhero />
       <InteractiveFaqAccordion
         faqs={[

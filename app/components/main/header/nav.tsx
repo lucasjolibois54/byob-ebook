0f9169405868774672ff2c660ffd8f8a12795718
lucasjolibois54 <lucasjolibois@icloud.com>
2025-01-28 22:37:51 +0100

    Update nav.tsx

--- app/components/main/header/nav.tsx
+++ app/components/main/header/nav.tsx
@@ -7,12 +7,12 @@ export default function Nav() {
 <nav className="fixed w-full top-0 z-30 bg-[#18181A] backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-700">
   <div className="max-w-7xl mx-auto px-4 text-white">
     <div className="flex items-center justify-between h-16">
-      <span className="text-2xl  font-semibold">Logo</span>
+      <span className="text-2xl  font-semibold">SelvKlip</span>
       <div className="flex space-x-4 ">
-        <a href="#">Dashboard</a>
         <a href="#">About</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
+        <a href="#">What you&apos;ll learn</a>
+        <a href="#">Reviews</a>
+        <a href="#">FAQ</a>
       </div>
       <HoverBorderButton />
     </div>

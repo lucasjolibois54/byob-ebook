2ba86210a278e46e72e850c9f5552e9ea3077187
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2025-01-29 16:30:02 +0100

    reviews update

--- app/components/Home/review/Review.tsx
+++ app/components/Home/review/Review.tsx
@@ -3,21 +3,23 @@ import LoopingCarousel from "./LoopingCarousel";
 const Review = () => {
   const reviews = [
     {
-      name: "Jill",
-      username: "@jill",
-      body: "This changed how I think about design. Brilliant work!",
+      name: "ibtesam hussain",
+      body: "I had a great experience at BecomeYourOwnBarber! As someone who had never even held a trimmer before, I got exactly the help I needed to start my journey of cutting my own hair.",
       img: "https://avatar.vercel.sh/jill",
     },
     {
-      name: "Jack",
-      username: "@jack",
-      body: "Absolutely blown away by the attention to detail. Fantastic!",
+      name: "Memis Goz",
+      body: "BecomeYourOwnBarber gave me excellent training in cutting a fade. Highly recommended",
       img: "https://avatar.vercel.sh/jack",
     },
     {
-      name: "Jane",
-      username: "@jane",
-      body: "The quality and thought put into this are remarkable. Highly impressed.",
+      name: "Gago Topal",
+      body: "I cut my own hair now and couldn't be happier!",
+      img: "https://avatar.vercel.sh/jane",
+    },
+    {
+      name: "Qasim",
+      body: "Definitely the best journey I've been on.I struggle to find time to go to the barber with my busy schedule. After discovering BecomeYourOwnBarber, I realized I could save both money and time in the long run.",
       img: "https://avatar.vercel.sh/jane",
     },
     // Add more reviews here

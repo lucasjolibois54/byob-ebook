365beb514aca4998edf27575450af7c9315860ef
lucasjolibois54 <lucasjolibois@icloud.com>
2025-02-04 19:52:31 +0100

    Update layout.tsx

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./../globals.css";
 import Nav from "../components/main/header/nav";
-//import Footer from "../components/main/footer/footer";
+import Footer from "../components/main/footer/footer";
 import MobileNav from "../components/main/header/mobilenav";
 
 const geistSans = Geist({
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Nav />
         <MobileNav />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
